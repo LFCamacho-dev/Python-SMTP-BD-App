dcdd18e0cc1aed53ad10bb50c53503177a0e2bb6
Luis Fernando Camacho <luisfernandoca@hotmail.com>
2022-01-26 13:54:24 -0600

    Reading and finding people with birthdays on today's date. Modifying letter templates with the name of the person with a birthday and sending them a personalized email. Only first match gets email

--- main.py
+++ main.py
@@ -1,33 +1,42 @@
 import random
+import os.path
 import smtplib
 import datetime as dt
+import pandas as pandas
 
-now = dt.datetime.now()
-day_of_week = now.weekday()
-
-my_email = "lfcamachodev@gmail.com"
-my_password = input("Password: ")
+# region MAIL AUTH
+email = "lfcamachodev@gmail.com"
+password = input("Enter Password: ")
 SMTP_SERVER = "smtp.gmail.com"
 PORT = 587
 SSL_PORT = 465
+# endregion MAIL AUTH
 
+now = dt.datetime.now()
+day_of_week = now.weekday()
+letters = os.listdir("letter_templates")
 
-if day_of_week == 2:
+birthday_data = pandas.read_csv("birthdays.csv")
+found = birthday_data.loc[(birthday_data['month'] == now.month) & (birthday_data['day'] == now.day)]
+found_name = found.name.values[0]
+found_email = found.email.values[0]
+
+if not found.empty:
+    chosen_letter = random.choice(letters)
+    with open(f"letter_templates/{chosen_letter}", "r") as let:
+        letter_data = let.read()
+        final_letter = letter_data.replace("[NAME]", found_name)
+
     with open("quotes.txt", "r") as file:
-        quotes = (file.readlines())
+        quotes = file.readlines()
         selected_quote = random.choice(quotes)
 
-    with smtplib.SMTP_SSL(SMTP_SERVER, SSL_PORT) as connection:
-        # connection.starttls() # used without SSL
-        connection.login(user=my_email, password=my_password)
-        connection.sendmail(from_addr=my_password,
-                            to_addrs="luisfernandoca@yahoo.com,lfcamachodev@gmail.com",
-                            msg=f"Subject: Quote Of The Day\n\n "
-                                f"{selected_quote}")
-
-        print(selected_quote + " was sent")
-
-
-
-
-
+    with smtplib.SMTP_SSL(SMTP_SERVER, SSL_PORT) as connection:  # connection.starttls() # used without SSL
+        connection.login(user=email, password=password)
+        connection.sendmail(
+            from_addr=email,
+            to_addrs=f"{found_email}",
+            msg=f"Subject: Happy Birthday {found_name}!\n\n "
+                f"{final_letter}\n\n\n"
+                f"----------------------------\n"
+                f"{selected_quote}")
