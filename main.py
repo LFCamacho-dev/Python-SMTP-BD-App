bcafb15847ab426436fdd6b8a75a1ddc04c51b15
Luis Fernando Camacho <luisfernandoca@hotmail.com>
2022-01-26 01:06:19 -0600

    Reading quotes from list in txt file, sending email with random quote from that list

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import random
 import smtplib
+import datetime as dt
 
+now = dt.datetime.now()
+day_of_week = now.weekday()
 
 my_email = "lfcamachodev@gmail.com"
 my_password = input("Password: ")
@@ -7,13 +11,23 @@ SMTP_SERVER = "smtp.gmail.com"
 PORT = 587
 SSL_PORT = 465
 
-with smtplib.SMTP_SSL(SMTP_SERVER, SSL_PORT) as connection:
-    # connection.starttls() # used without SSL
-    connection.login(user=my_email, password=my_password)
 
-    connection.sendmail(from_addr=my_password,
-                        to_addrs="luisfernandoca@yahoo.com",
-                        msg="Subject: Hola con SSL!\n\n "
-                            "This is the body of my email")
+if day_of_week == 2:
+    with open("quotes.txt", "r") as file:
+        quotes = (file.readlines())
+        selected_quote = random.choice(quotes)
+
+    with smtplib.SMTP_SSL(SMTP_SERVER, SSL_PORT) as connection:
+        # connection.starttls() # used without SSL
+        connection.login(user=my_email, password=my_password)
+        connection.sendmail(from_addr=my_password,
+                            to_addrs="luisfernandoca@yahoo.com,lfcamachodev@gmail.com",
+                            msg=f"Subject: Quote Of The Day\n\n "
+                                f"{selected_quote}")
+
+        print(selected_quote + " was sent")
+
+
+
 
 
