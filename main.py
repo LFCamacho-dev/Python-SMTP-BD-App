e5992672575bc1c5d7487d89cb5911aa1e333b23
Luis Fernando Camacho <luisfernandoca@hotmail.com>
2022-01-26 23:12:05 -0600

    every contact in the birthdays.csv that have a birthday 'today', get a Birthday Email

--- main.py
+++ main.py
@@ -5,38 +5,40 @@ import datetime as dt
 import pandas as pandas
 
 # region MAIL AUTH
-email = "lfcamachodev@gmail.com"
-password = input("Enter Password: ")
+email = "l*********@gmail.com"
+password = "********"
 SMTP_SERVER = "smtp.gmail.com"
 PORT = 587
 SSL_PORT = 465
 # endregion MAIL AUTH
 
 now = dt.datetime.now()
-day_of_week = now.weekday()
+today_date = f"{now.month} - {now.day}"
 letters = os.listdir("letter_templates")
 
-birthday_data = pandas.read_csv("birthdays.csv")
-found = birthday_data.loc[(birthday_data['month'] == now.month) & (birthday_data['day'] == now.day)]
-found_name = found.name.values[0]
-found_email = found.email.values[0]
+birthday_data = pandas.read_csv("birthdays.csv").to_dict(orient="records")
+for bd in birthday_data:
+    birth_date = f"{bd['month']} - {bd['day']}"
 
-if not found.empty:
-    chosen_letter = random.choice(letters)
-    with open(f"letter_templates/{chosen_letter}", "r") as let:
-        letter_data = let.read()
-        final_letter = letter_data.replace("[NAME]", found_name)
+    if today_date == birth_date:
 
-    with open("quotes.txt", "r") as file:
-        quotes = file.readlines()
-        selected_quote = random.choice(quotes)
+        chosen_letter = random.choice(letters)
+        with open(f"letter_templates/{chosen_letter}", "r") as let:
+            letter_data = let.read()
+            final_letter = letter_data.replace("[NAME]", bd['name'])
 
-    with smtplib.SMTP_SSL(SMTP_SERVER, SSL_PORT) as connection:  # connection.starttls() # used without SSL
-        connection.login(user=email, password=password)
-        connection.sendmail(
-            from_addr=email,
-            to_addrs=f"{found_email}",
-            msg=f"Subject: Happy Birthday {found_name}!\n\n "
-                f"{final_letter}\n\n\n"
-                f"----------------------------\n"
-                f"{selected_quote}")
+        with open("quotes.txt", "r") as file:
+            quotes = file.readlines()
+            selected_quote = random.choice(quotes)
+
+        with smtplib.SMTP_SSL(SMTP_SERVER, SSL_PORT) as connection:  # connection.starttls() # used without SSL
+            connection.login(user=email, password=password)
+            connection.sendmail(
+                from_addr=email,
+                to_addrs=f"{bd['email']}",
+                msg=f"Subject: Happy Birthday {bd['name']}!\n\n "
+                    f"{final_letter}\n\n\n"
+                    f"----------------------------\n"
+                    f"{selected_quote}")
+
+        print(f"Birthday email sent to {bd['name']}, ({bd['year']} - {birth_date})")
